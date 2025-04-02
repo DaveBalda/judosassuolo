9b83218610d7f55fc31d0dc4b8fbbe555fd63863
DaveBalda <davide.baldacini@gmail.com>
2025-04-02 14:54:59 +0200

    Finished Corsi

--- src/pages/Corsi/Corsi.tsx
+++ src/pages/Corsi/Corsi.tsx
@@ -5,19 +5,14 @@ function Corsi() {
     <PageLayout>
       <div className="flex flex-col">
         <div className="h-fit flex flex-col gap-10 items-center py-24 px-52 bg-gray-200 shadow-2xl">
-          <div className="flex flex-col items-center gap-3">
-            <h1 className="text-5xl font-bold font-oswald pt-2 pb-3.5 px-4 bg-green-900 rounded-3xl text-white">
-              Junior
-            </h1>
-            <p className="text-xl font-oswald">
-              per i bambini di 4-5 anni di età
-            </p>
-          </div>
+          <h1 className="text-5xl font-bold font-oswald pt-2 pb-3.5 px-4 bg-green-900 rounded-3xl text-white">
+            Elementari
+          </h1>
           <hr className="w-full border-gray-300" />
           <div className="flex flex-row gap-24 items-center">
             <div className="rounded-xl w-[700px] max-h-[700px] h-auto overflow-hidden border">
               <img
-                src="/images/corsi/garabimbi.jpeg"
+                src="/images/corsi/bimbimedals.jpeg"
                 width="700px"
                 alt="Corso Junior"
               />
@@ -28,32 +23,109 @@ function Corsi() {
                   Giorni e Orari
                 </h1>
                 <p className="text-xl font-oswald">
-                  Il corso si terrà il <strong>martedì</strong> e il{" "}
-                  <strong>giovedì</strong> dalle ore <strong>17:30</strong> alle
-                  ore <strong>18:30</strong>
+                  Il corso si terrà il <strong>lunedì</strong> e il{" "}
+                  <strong>mercoledì</strong> dalle ore <strong>17:00</strong>{" "}
+                  alle ore <strong>18:00</strong>
                 </p>
               </div>
               <div className="flex flex-col gap-2 items-center">
                 <h1 className="text-3xl font-bold font-oswald">
                   Informazioni sul corso
                 </h1>
-                <div className="flex flex-col items-center gap-5">
-                  <p className="text-xl font-oswald">
-                    L'attività proposta sarà un percorso neuro-motorio{" "}
-                    <strong>"MoviMente"</strong> volto ad incentivare lo
-                    sviluppo intellettivo e motorio dei bambini di 4 o 5 anni di
-                    età. Questo percorso, negli anni, si è dimostrato{" "}
-                    <strong>molto efficace</strong> nella crescita di tutti i
-                    bambini che lo hanno sperimentato.
-                  </p>
-                  <a
-                    href="https://www.propatriajudo.it/presentazione-progetto-judogiocando"
-                    target="_blank"
-                    className="font-gidole bg-red-500 hover:bg-red-600 px-6 py-3 text-xl rounded-lg text-white"
-                  >
-                    Scopri di più
-                  </a>
-                </div>
+                <p className="text-xl font-oswald">
+                  Il corso insegnerà ai bambini dapprima le{" "}
+                  <strong>basi del Judo</strong>, poi le tecniche più avanzate,
+                  riadattando il tutto all'<strong>età dell'iscritto</strong>.
+                  Il tutto verrà effettuato in un ambiente controllato e
+                  professionale.
+                  <br />
+                  <br />
+                  <em>Anche i bambini di 4-5 anni sono i benvenuti!</em>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-fit flex flex-col gap-10 items-center py-24 px-52 shadow-2xl">
+          <h1 className="text-5xl font-bold font-oswald pt-2 pb-3.5 px-4 bg-green-900 rounded-3xl text-white">
+            Avanzati
+          </h1>
+          <hr className="w-full border-gray-300" />
+          <div className="flex flex-row gap-24 items-center">
+            <div className="rounded-xl w-[700px] max-h-[700px] h-auto overflow-hidden border">
+              <img
+                src="/images/corsi/bimbi.jpeg"
+                width="700px"
+                alt="Corso Junior"
+              />
+            </div>
+            <div className="flex flex-col w-full justify-center items-center gap-20 text-center px-10">
+              <div className="flex flex-col gap-2 items-center">
+                <h1 className="text-3xl font-bold font-oswald">
+                  Giorni e Orari
+                </h1>
+                <p className="text-xl font-oswald">
+                  Il corso si terrà il <strong>lunedì</strong>, il{" "}
+                  <strong>mercoledì</strong> e il <strong>venerdì</strong> dalle
+                  ore <strong>18:30</strong> alle ore <strong>19:30</strong>
+                </p>
+              </div>
+              <div className="flex flex-col gap-2 items-center">
+                <h1 className="text-3xl font-bold font-oswald">
+                  Informazioni sul corso
+                </h1>
+                <p className="text-xl font-oswald">
+                  Il corso è adatto per coloro che frequentano già le scuole
+                  medie e permette di <strong>approfondire</strong>{" "}
+                  ulteriormente la conoscenza della disciplina, integrando anche
+                  tecniche più elaborate e un{" "}
+                  <strong>allenamento fisico</strong> più intenso, sempre però
+                  proporzionato alle abilità fisiche dell'iscritto.
+                  <br />
+                  <br />
+                  <em>Nessuno viene lasciato indietro!</em>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="h-fit flex flex-col gap-10 items-center py-24 px-52 bg-emerald-300/40 shadow-2xl">
+          <h1 className="text-5xl font-bold font-oswald pt-2 pb-3.5 px-4 bg-green-900 rounded-3xl text-white">
+            Adulti e agonisti
+          </h1>
+          <hr className="w-full border-emerald-300/60" />
+          <div className="flex flex-row gap-24 items-center">
+            <div className="rounded-xl w-[700px] max-h-[700px] h-auto overflow-hidden border">
+              <img
+                src="/images/corsi/gruppograndi.jpeg"
+                width="700px"
+                alt="Corso Junior"
+              />
+            </div>
+            <div className="flex flex-col w-full justify-center items-center gap-20 text-center px-10">
+              <div className="flex flex-col gap-2 items-center">
+                <h1 className="text-3xl font-bold font-oswald">
+                  Giorni e Orari
+                </h1>
+                <p className="text-xl font-oswald">
+                  Il corso si terrà il <strong>lunedì</strong>, il{" "}
+                  <strong>mercoledì</strong> e il <strong>venerdì</strong> dalle
+                  ore <strong>19:30</strong> alle ore <strong>20:30</strong>
+                </p>
+              </div>
+              <div className="flex flex-col gap-2 items-center">
+                <h1 className="text-3xl font-bold font-oswald">
+                  Informazioni sul corso
+                </h1>
+                <p className="text-xl font-oswald">
+                  Il corso è adatto a tutti coloro che vogliono praticare il
+                  Judo a livello <strong>amatoriale</strong> o{" "}
+                  <strong>agonistico</strong>. Verranno praticate tecniche di
+                  livello base, tecnico di livello medio e tecniche difficili,
+                  tenendo ovviamente conto del livello del singolo atleta. Sarà
+                  possibile anche <strong>mettersi alla prova</strong> e
+                  partecipare a competizioni da tutta Italia.
+                </p>
               </div>
             </div>
           </div>
